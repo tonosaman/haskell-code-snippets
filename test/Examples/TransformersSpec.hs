6a305b307ee08008ec218fb68c3531b589178be4
tonosaman <uploader360@gmail.com>
2014-06-11 16:04:48 +0900

    refactoring for travis

--- test/Examples/TransformersSpec.hs
+++ test/Examples/TransformersSpec.hs
@@ -6,31 +6,30 @@ import qualified Data.Map as Map
 spec :: Spec
 spec = do
     describe "eval0" $ evaluation_context_with eval0
-    describe "eval1" $ evaluation_context_with (\ env exp -> runEval1 $ eval1 env exp)
-    describe "eval2" $ evaluation_context_with $
-      \ env exp -> case runEval2 (eval2 env exp) of Right iv -> iv
-    describe "eval3" $ evaluation_context_with $
-      \ env exp -> case runEval3 (eval3 exp) env of Right iv -> iv
-    describe "eval4" $ evaluation_context_with $
-      \ env exp -> case runEval4 (eval4 exp) env 0 of (Right iv, st) -> iv
+    describe "eval1" $ evaluation_context_with (\ ev ex -> runEval1 $ eval1 ev ex)
+    describe "eval2" $ evaluation_context_with . normarize $ \ ev ex -> runEval2 $ eval2 ev ex
+    describe "eval3" $ evaluation_context_with . normarize $ \ ev ex -> runEval3 (eval3 ex) ev
+    describe "eval4" $ evaluation_context_with . normarize $ \ ev ex -> fst $ runEval4 (eval4 ex) ev 0
     where
+      normarize :: (Env -> Exp -> Either a Value) -> (Env -> Exp -> Value)
+      normarize runEval = \ev -> either (const $ IntVal 0) id . runEval ev
       evaluation_context_with :: (Env -> Exp -> Value) -> Spec
       evaluation_context_with eval = do
         it "evaluate Lit" $
           eval Map.empty (Lit 1) `shouldBe` IntVal 1
         it "evaluate Var" $ let
-          env = Map.fromList [("x", IntVal 2)]
-          exp = Var "x"
-          in eval env exp `shouldBe` IntVal 2
+          ev = Map.fromList [("x", IntVal 2)]
+          ex = Var "x"
+          in eval ev ex `shouldBe` IntVal 2
         it "evaluate Plus" $  let
-          exp = Lit 1 `Plus` Lit 2
-          in eval Map.empty exp `shouldBe` IntVal 3
+          ex = Lit 1 `Plus` Lit 2
+          in eval Map.empty ex `shouldBe` IntVal 3
         it "evaluate Abs" $ let
-          exp = Abs "x" $ Var "x" `Plus` Lit 2
-          in eval (Map.empty) exp `shouldBe` (FunVal Map.empty "x" $ Var "x" `Plus` Lit 2)
+          ex = Abs "x" $ Var "x" `Plus` Lit 2
+          in eval (Map.empty) ex `shouldBe` (FunVal Map.empty "x" $ Var "x" `Plus` Lit 2)
         it "evaluate App" $ let
-          exp = App (Abs "x" $ Var "x" `Plus` Lit 2) (Lit 1)
-          in eval Map.empty exp `shouldBe` IntVal 3
+          ex = App (Abs "x" $ Var "x" `Plus` Lit 2) (Lit 1)
+          in eval Map.empty ex `shouldBe` IntVal 3
 
 main :: IO ()
 main = hspec spec
--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,13 +1,11 @@
 module SpecHelper
            ( module Test.Hspec
            , module Codec.Base64
-           , module Compiler.Utils.Bag
            , module Examples.Transformers
            , module Hassium.Args
            ) where
 
 import Test.Hspec
 import Codec.Base64
-import Compiler.Utils.Bag
 import Examples.Transformers
 import Hassium.Args
